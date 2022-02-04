f3328b5c14547f3376f964765ae409ab9416fb83
galvains <51203yarik@gmail.com>
2022-02-04 19:19:58 +0300

    init (1 часть, логика второй работают)

---
+++ laba_2/functions.js
@@ -0,0 +1,54 @@
+function ob1(){
+	var Person = {
+		user1: {
+			'name': 'Сергей',
+			'surname': 'Жуков',	
+			'grants': 'получает'
+		},
+		user2: {
+			'name': 'Светлана',
+			'surname': 'Иванова',
+			'grants': 'не получает'
+		},
+		user3: {
+			'name': 'Мария',
+			'surname': 'Борисова',
+			'grants': 'получает'
+		},
+		user4: {
+			'name': 'Евгений',
+			'surname': 'Носов',
+			'grants': 'не получает'
+		}
+	}
+
+	var otv = '';
+	for (var key in Person) {
+		if (Person[key].grants == 'получает') {
+			otv += "Имя: " + Person[key].name + '</br>';
+			otv += "Фамилия: " + Person[key].surname + '</br>';
+			otv += "Стипендия: " + Person[key].grants + '</br></br>';
+		}
+
+		document.getElementById('result1').innerHTML = otv;
+	}
+}
+
+function ob2() {
+	var a = parseInt(document.getElementById('a').value);
+	var b = parseInt(document.getElementById('b').value);
+	
+	var str = String(a);
+		
+	sum = 0;
+	
+		for (var i of str) {
+			sum += Number(i);
+		}
+	
+
+	document.getElementById('result2').innerHTML = sum;
+}
+
+
+
