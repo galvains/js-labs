4c4a72348d629fc879977041df34099851ec6ccb
galvains <51203yarik@gmail.com>
2022-04-06 22:54:03 +0300

    sort_date and surname

--- laba_11/functions.js
+++ laba_11/functions.js
@@ -1,40 +1,67 @@
-function ob1(){
+function lab(){
+	arr = [];
+	arr[0] = {surname:'Ivanov', age: 'st. Gurova, h. 32', date: 'March 2, 2019'};
+	arr[1] = {surname:'Petrov', age: 'st. Zavodskaya, h. 2', date: 'April 23, 2020'};
+	arr[2] = {surname:'Sidorov', age: 'st. Artema, h. 56', date: 'December 5, 2021'};
+	arr[3] = {surname:'Ganzha', age: 'st. Petrova, h. 1', date: 'June 4, 2027'};
 
-	//document.getElementById('result1').innerHTML =  re;
-}
+	arr[4] = {surname:'Ganzha', age: 'st. Petrova, h. 1', date: 'June 16, 2022'}; 
+	arr[5] = {surname:'Sidorov', age: 'st. Artema, h. 56', date: 'December 11, 2021'}; 
 
-function ob2(){
-	let n = document.getElementById('n').value;
-	let m = document.getElementById('m').value;
-	let x = []; out =''; res = [];
-	
 
-	try{
-		if(n != Number(n) | m != Number(m)){
-			throw new Error('неверные символы!')
-		}
-		else{
-			for (let i = 0; i < m; i++){
-				x[i] = [];
-					for (let k = 0; k < n; k++){			
-						x[i][k] = Math.floor(Math.random() * 10);
-							if(i > k){
-								res.push(x[i][k]);
-							}
-						out = out + ' ' + x[i][k];
-					}
-				out = out + '<br>';
-			}
-			document.getElementById('result2').innerHTML = out + '<br>' + res.sort();
+	//arr = Array.from(new Set(array));
+
+	let otv = ''; 
+
+	for(let i in arr){
+		otv += '<br>';
+		for(let k in arr[i]){
+			otv += arr[i][k] + ' | ';
 		}
 	}
-			
-	catch (Error){
-		document.getElementById('result2').innerHTML = Error.name + ': ' + Error.message;
+	document.getElementById('result').innerHTML =  otv;
+}
 
+function sort_surname(){
+	arr.sort(function(a, b){
+		let surnameA = a.surname.toLowerCase(), surnameB = b.surname.toLowerCase()
+		if (surnameA < surnameB){
+			return -1
+		}
+		if (surnameA > surnameB){
+			return 1
+		}
+		return 0
+	})
+	let otv = ''; 
+
+	for(let i in arr){
+		otv += '<br>';
+		for(let k in arr[i]){
+			otv += arr[i][k] + ' | ';
+		}
 	}
-	//document.getElementById('result2').innerHTML = typeof n;
+	document.getElementById('result').innerHTML =  otv;
+}
+
+function sort_date(){
+	arr.sort(function(a, b){
+		let dateA = new Date(a.date), dateB = new Date(b.date);
+		return dateA - dateB;
+	})
+	let otv = ''; 
+
+	for(let i in arr){
+		otv += '<br>';
+		for(let k in arr[i]){
+			otv += arr[i][k] + ' | ';
+		}
+	}
+	document.getElementById('result').innerHTML =  otv;
 }
 
 
+//function ob2(){
+//	let n = document.getElementById('n').value;
+//}
 
