f2f8b2e7294489ee74112c74bc1d3928e2507a56
galvains <51203yarik@gmail.com>
2022-04-09 16:36:28 +0300

    complete

--- laba_11/functions.js
+++ laba_11/functions.js
@@ -4,25 +4,21 @@ function lab(){
 	arr[1] = {surname:'Petrov', age: 'st. Zavodskaya, h. 2', date: 'April 23, 2020'};
 	arr[2] = {surname:'Sidorov', age: 'st. Artema, h. 56', date: 'December 5, 2021'};
 	arr[3] = {surname:'Ganzha', age: 'st. Petrova, h. 1', date: 'June 4, 2027'};
-
-	arr[4] = {surname:'Ganzha', age: 'st. Petrova, h. 1', date: 'June 16, 2022'}; 
-	arr[5] = {surname:'Sidorov', age: 'st. Artema, h. 56', date: 'December 11, 2021'}; 
-
-
-	//arr = Array.from(new Set(array));
+	arr[4] = {surname:'Ganzha', age: 'st. Petrova, h. 1', date: 'June 4, 2027'}; 
+	arr[5] = {surname:'Sidorov', age: 'st. Artema, h. 56', date: 'December 5, 2021'}; 
 
 	let otv = ''; 
 
 	for(let i in arr){
-		otv += '<br>';
 		for(let k in arr[i]){
 			otv += arr[i][k] + ' | ';
 		}
+		otv += '<br>';
 	}
 	document.getElementById('result').innerHTML =  otv;
 }
 
-function sort_surname(){
+function sort_delete(){
 	arr.sort(function(a, b){
 		let surnameA = a.surname.toLowerCase(), surnameB = b.surname.toLowerCase()
 		if (surnameA < surnameB){
@@ -33,14 +29,25 @@ function sort_surname(){
 		}
 		return 0
 	})
-	let otv = ''; 
+	let otv = '';
+	let set = new Set(arr.map(JSON.stringify));
+	let new_arr = Array.from(set).map(JSON.parse);
 
-	for(let i in arr){
-		otv += '<br>';
-		for(let k in arr[i]){
-			otv += arr[i][k] + ' | ';
+	//for(let i in new_arr){
+	//	otv += i + '|';
+	//}
+
+	//for(let i = 0; i < arr.length; i++){
+	//	obj[arr[i]['surname']] = arr[i];
+	//}
+
+	for(let i in new_arr){
+		for(let j in new_arr[i]){
+			otv += new_arr[i][j] + ' | ';
 		}
+		otv += '<br>';
 	}
+
 	document.getElementById('result').innerHTML =  otv;
 }
 
@@ -52,16 +59,10 @@ function sort_date(){
 	let otv = ''; 
 
 	for(let i in arr){
-		otv += '<br>';
 		for(let k in arr[i]){
 			otv += arr[i][k] + ' | ';
 		}
+		otv += '<br>';
 	}
 	document.getElementById('result').innerHTML =  otv;
 }
-
-
-//function ob2(){
-//	let n = document.getElementById('n').value;
-//}
-
