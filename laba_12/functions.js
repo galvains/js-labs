e73cf12d4cf8f7c0ef724a41136ab6886f0e7ca0
Yaroslav Ganzha <51203yarik@gmail.com>
2022-04-21 18:22:53 +0300

    1 q complete

---
+++ laba_12/functions.js
@@ -0,0 +1,56 @@
+function rec(){
+	let n = parseInt(document.getElementById('a').value);
+	let res = '';
+
+	F(n);
+	function F(n){
+		res += n;
+		if(n < 4){
+			F(n + 1);
+			F(n + 3);
+		}
+	}
+
+	document.getElementById('result1').innerHTML = res;
+}
+
+//function rec2(){
+//	let n = parseInt(document.getElementById('b').value);
+//	let res; //i; k; c = 2; z = 1; p = 1;
+//
+//
+//	if (n == 1){
+//		res = 2;
+//	}
+//	else if (n % 2){
+//		res = (n + 1) * rec2(n - 1) / n;
+//	}
+//	else{
+//		res = n * rec2(n - 1) / (n + 1);
+//	}
+//	
+//
+//	document.getElementById('result2').innerHTML = res;
+//}
+
+function ob2(){
+	let n = parseInt(document.getElementById('b').value);
+	res = 0; 
+
+	rec2(n);
+	function rec2(n){
+		//res += n;
+		if (n == 1){
+			res = 2;
+		}
+		else if (n % 2){
+			res = (n + 1) * rec2(n - 1) / n;
+		}
+		else{
+			res = n * rec2(n - 1) / (n + 1);
+		}
+	}
+	
+
+	document.getElementById('result2').innerHTML = res;
+}
